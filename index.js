058ccd8c0936d7c69daf4570c2327d4135cb01c0
Dylen Gitaka <dylen.gitaka@student.moringaschool.com>
2024-03-26 08:53:19 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+// Function expression
+const divide = function(a, b) {
+    return a / b;
+  };
+
+  // Arrow function
+  const square = (num) => {
+    return num * num;
+  };
+
+  // Arrow function
+  const add = (a, b) => {
+    return a + b;
+  };
+
+  // Export the functions
+  module.exports = { divide, square, add };
